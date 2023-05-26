3d747a7b73bd11d6370c86c7d9db7c42922bc22d
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2023-05-26 18:57:24 +0800

    Add SplashScreen, Navigation and BottomSheet

--- app/src/main/java/com/bangkit/mocca/MainActivity.kt
+++ app/src/main/java/com/bangkit/mocca/MainActivity.kt
@@ -1,7 +1,17 @@
 package com.bangkit.mocca
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.ImageView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.bangkit.mocca.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +23,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.hide()
+
+        val navHostController = findNavController(R.id.nav_host_fragment_container)
+        binding.bottomNavigationView.setupWithNavController(navHostController)
         binding.bottomNavigationView.background = null
         binding.bottomNavigationView.menu.getItem(2).isEnabled = false
+
+        binding.fabAdd.setOnClickListener {
+            showBottomSheet()
+        }
+    }
+
+    private fun showBottomSheet() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.bottom_sheet_layout)
+
+        val btnClose: ImageView = dialog.findViewById(R.id.btn_close)
+
+        btnClose.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window?.attributes?.windowAnimations = R.style.BottomDialogAnimation
+        dialog.window?.setGravity(Gravity.BOTTOM)
     }
 }
--- app/src/main/java/com/bangkit/mocca/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/bangkit/mocca/ui/splash/SplashActivity.kt
@@ -1,8 +1,11 @@
 package com.bangkit.mocca.ui.splash
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.bangkit.mocca.MainActivity
 import com.bangkit.mocca.R
 import com.bangkit.mocca.databinding.ActivitySplashBinding
 
@@ -16,6 +19,17 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.hide()
 
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splash_timer)
+    }
+
+    companion object {
+        const val splash_timer = 2000L
     }
 }
