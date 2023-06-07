f7e06f42c89f2f7436bc818dc728ba06eab5f5dc
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2023-06-07 20:49:21 +0800

    Add Dummy Data Transaction  and Helper for camera

---
+++ app/src/main/java/com/bangkit/mocca/model/TransactionModel.kt
@@ -0,0 +1,10 @@
+package com.bangkit.mocca.model
+
+data class TransactionModel (
+    val id: Int,
+    val description: String,
+    val date: String,
+    val price: Double,
+    val category: String,
+    val type: String
+)
---
+++ app/src/main/java/com/bangkit/mocca/utils/Helper.kt
@@ -0,0 +1,49 @@
+package com.bangkit.mocca.utils
+
+import android.app.Application
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.os.Environment
+import com.bangkit.mocca.R
+import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+private const val FILENAME_FORMAT = "dd-MMM-yyyy"
+
+val timeStamp: String = SimpleDateFormat(
+    FILENAME_FORMAT,
+    Locale.US
+).format(System.currentTimeMillis())
+
+fun createCustomTempFile(context: Context): File {
+    val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    return File.createTempFile(timeStamp, ".jpg", storageDir)
+}
+
+fun createFile(application: Application): File {
+    val mediaDir = application.externalMediaDirs.firstOrNull()?.let {
+        File(it, application.resources.getString(R.string.app_name)).apply { mkdirs() }
+    }
+
+    val outputDirectory = if (
+        mediaDir != null && mediaDir.exists()
+    ) mediaDir else application.filesDir
+
+    return File(outputDirectory, "$timeStamp.jpg")
+}
+
+fun rotateFile(file: File, isBackCamera: Boolean = false) {
+    val matrix = Matrix()
+    val bitmap = BitmapFactory.decodeFile(file.path)
+    val rotation = if (isBackCamera) 90f else -90f
+    matrix.postRotate(rotation)
+    if (!isBackCamera) {
+        matrix.postScale(-1f, 1f, bitmap.width / 2f, bitmap.height / 2f)
+    }
+    val result = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+    result.compress(Bitmap.CompressFormat.JPEG, 100, FileOutputStream(file))
+}
